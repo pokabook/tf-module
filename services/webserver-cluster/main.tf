48ec4b5318c24dfe9975cecb386b612a687e10a2
poka <khs0826@dsm.hs.kr>
2023-06-03 19:18:10 +0900

    tag variables

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -37,6 +37,19 @@ resource "aws_autoscaling_group" "example" {
     value               = var.cluster_name
     propagate_at_launch = true
   }
+
+  dynamic "tag" {
+    for_each = {
+    for key, value in var.custom_tags :
+    key => upper(value)
+    if key != "Name"
+    }
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
 }
 
 resource "aws_security_group" "instance" {
